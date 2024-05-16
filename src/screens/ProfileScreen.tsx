2a156e3934a42eb7f5aa57772e730691f111f040
Justin <justin@Justins-MacBook-Pro.local>
2024-05-16 18:43:41 +0700

    finalized profile screen

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -5,79 +5,75 @@ import { Colors } from "@values";
 import { AppBar, CredentialInput, Button } from '@components';
 
 export default function ProfileScreen() {
+  const [name, setName] = useState('Kartika Sari');
+  const [number, setNumber] = useState('081212336163');
+  const [email, setEmail] = useState('kartik@mail.com');
+
+  const [initiallName, setinitiallName] = useState(name);
+  const [initiallNumber, setinitiallNumber] = useState(number);
+  const [initiallEmail, setinitiallEmail] = useState(email);
+
+  const [isChanged, setIsChanged] = useState(false);
+
   const [fontsLoaded, fontError] = useFonts({
     'Segoe UI': require('@fonts/Segoe UI.ttf'),
     'Segoe UI Bold': require('@fonts/Segoe UI Bold.ttf'),
-});
+  });
 
-const [name, setName] = useState('Kartika Sari');
-const [number, setNumber] = useState('081212336163');
-const [email, setEmail] = useState('kartik@mail.com');
+  if (!fontsLoaded && !fontError) {
+      return null;
+  };
 
-// State to store the original values
-const [originalName, setOriginalName] = useState(name);
-const [originalNumber, setOriginalNumber] = useState(number);
-const [originalEmail, setOriginalEmail] = useState(email);
-
-// State to track if there are changes in the input fields
-const [isChanged, setIsChanged] = useState(false);
-
-if (!fontsLoaded && !fontError) {
-    return null;
-}
-
-return (
-    <>
+  return (
+      <>
         <AppBar />
         <View style={styles.container}>
             <Text style={styles.label}>Name</Text>
             <CredentialInput 
-                value={name} 
-                onChangeText={(text) => { setName(text); setIsChanged(true); }} 
-                style={styles.inputField} 
+              value={name} 
+              onChangeText={(text) => { setName(text); setIsChanged(true); }} 
+              style={styles.inputField} 
             />
             <Text style={styles.label}>Number</Text>
             <CredentialInput 
-                value={number} 
-                onChangeText={(text) => { setNumber(text); setIsChanged(true); }} 
-                style={styles.inputField} 
+              value={number} 
+              onChangeText={(text) => { setNumber(text); setIsChanged(true); }} 
+              style={styles.inputField} 
             />
             <Text style={styles.label}>Email</Text>
             <CredentialInput 
-                value={email} 
-                onChangeText={(text) => { setEmail(text); setIsChanged(true); }} 
-                style={styles.inputField} 
+              value={email} 
+              onChangeText={(text) => { setEmail(text); setIsChanged(true); }} 
+              style={styles.inputField} 
             />
             <Button 
-                title='Save' 
-                style={[styles.saveButton, !isChanged && styles.disabledButton]} 
-                disabled={!isChanged}
-                onPress={() => {
-                    setOriginalName(name);
-                    setOriginalNumber(number);
-                    setOriginalEmail(email);
-                    setIsChanged(false);
-                }}
+              title='Save' 
+              style={[styles.saveButton, !isChanged && styles.disabledButton]} 
+              disabled={!isChanged}
+              onPress={() => {
+                  setinitiallName(name);
+                  setinitiallNumber(number);
+                  setinitiallEmail(email);
+                  setIsChanged(false);
+              }}
             />
-            {isChanged && (
-                <Button 
-                    title='Discard' 
-                    style={[styles.discardButton, !isChanged && styles.disabledButton]} 
-                    textStyle={[styles.discardButtonText, !isChanged && styles.disabledButtonText]}
-                    disabled={!isChanged}
-                    onPress={() => {
-                        setName(originalName);
-                        setNumber(originalNumber);
-                        setEmail(originalEmail);
-                        setIsChanged(false);
-                    }}
-                />
-            )}
+            <Button 
+              title='Discard' 
+              style={[styles.discardButton, !isChanged && styles.disabledButton]} 
+              textStyle={[styles.discardButtonText, !isChanged && styles.disabledButtonText]}
+              disabled={!isChanged}
+              onPress={() => {
+                  setName(initiallName);
+                  setNumber(initiallNumber);
+                  setEmail(initiallEmail);
+                  setIsChanged(false);
+              }}
+            />
             <Button title='Log Out' style={styles.logoutButton} textStyle={styles.logoutButtonText}/>
         </View>
     </>
-)
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -94,7 +90,7 @@ const styles = StyleSheet.create({
   inputField: {
     backgroundColor: Colors.grayLighter,
     borderRadius: 8,
-    padding: 16,
+    paddingHorizontal: 16,
     marginBottom: 13,
     borderBottomWidth: 0,
     fontFamily: 'Segoe UI Bold',
